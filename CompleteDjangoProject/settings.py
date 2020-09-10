1c60ca60cd7da6c2b33ea8a3f980280092ca72fb
PiasTanmoy <pias1600@gmail.com>
2020-09-10 19:24:58 +0600

    crispy form added

--- CompleteDjangoProject/settings.py
+++ CompleteDjangoProject/settings.py
@@ -37,8 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ProductManagement.apps.ProductmanagementConfig'
+    'ProductManagement.apps.ProductmanagementConfig',
+    'crispy_forms',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
