c6924f451deddcc827e3f12978ce254d080d741c
PiasTanmoy <pias1600@gmail.com>
2020-09-10 19:06:04 +0600

    mainapp

---
+++ CompleteDjangoProject/urls.py
@@ -0,0 +1,31 @@
+"""CompleteDjangoProject URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from ProductManagement import views as product_views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('products/', product_views.showProducts, name='products_list'),
+    path('products/<int:product_id>', product_views.showDetails, name='detail_view'),
+    path('upload/', product_views.uploadProducts, name='upload_product')
+]
+
+if settings.DEBUG == True:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
